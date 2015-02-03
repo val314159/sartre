36ecf5206933a73bfa019355ce3d108959abbd7d
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2015-02-03 13:28:09 -0800

    clean up code

--- static/examples/editor/app.js
+++ static/examples/editor/app.js
@@ -1,70 +1,50 @@
-
-function ping(){
-    var access_token = readCookie('access_token');
-    rpc_send("ping",["Hello, world"],function(data){
-	    console.log("PING RESPONSED TO!!!!"+str(data));
-	    document.getElementById('pong').innerHTML = "pong:"+access_token;
-	});
+function transmogrify(map,path,acc){
+    if (!map[path]) return;
+    var dirs = map[path][0];
+    var files= map[path][1];
+    $.each(dirs,function(ndx,val){
+	var nodes = transmogrify(map,path+'/'+val,[]);
+	acc.push({text:val+'/',nodes:nodes,
+		  data:{t:'d',p:path,f:val} });
+    });
+    $.each(files,function(ndx,val){
+	acc.push({ text: val,
+		   data:{t:'f',p:path,f:val,
+			 cb:function(){
+			     loadFile(path +'/'+ val);
+			 }}});
+    });
+    return acc;
 }
 
-function fs_load(filename,cb){
-    rpc_send("fs_load",[filename],function(data){
+var fs={
+    load:function(filename,cb){
+	rpc_send("fs_load",[filename],function(data){
 	    console.log("LOAD RESPONSED TO!!!!"+str(data));
 	    cb(data);
 	});
-}
-function fs_save(filename,data,cb){
-    rpc_send("fs_save",[filename,data],function(data){
+    },
+    save:function(filename,data,cb){
+	rpc_send("fs_save",[filename,data],function(data){
 	    console.log("SAVE RESPONSED TO!!!!"+str(data));
 	    cb(data);
 	});
-}
-function fs_walk() {
-    rpc_send("fs_walk",["fs"],function(data){
-	    var map = data.result;
-	    function findlist(path,acc){
-		if (!map[path]) return;
-		var dirs = map[path][0];
-		var files= map[path][1];
-		$.each(dirs,function(ndx,val){
-			LOG("D:"+path+'/'+val);
-			var nodes = findlist(path+'/'+val,[]);
-			acc.push({text:val+'/',nodes:nodes,
-				    data:{t:'d',p:path,f:val} });
-		    });
-		$.each(files,function(ndx,val){
-			LOG("F:"+path+'/'+val);
-			acc.push({ text: val, data:{t:'f',p:path,f:val,
-					cb:function(){
-					loadFile(path +'/'+ val);
-				    }}});
-		    });
-		return acc;
-	    }
-	    var lst = findlist('fs',[]);
-	    LOG("LST:"+str(lst));
-	    _tree = [{text:'Remote fs',nodes:lst}];
-	    regetTree();
+    },
+    walk:function(renderFn) {
+	rpc_send("fs_walk",["fs"],function(data){
+	    Tree = [{text:'Remote fs',nodes:transmogrify(data.result,'fs',[])}];
+	    renderFn();
 	});
-}
+    }
+};
 ///
 rpc_add_open(function(){
-	var access_token = readCookie('access_token');
-	document.getElementById('access_token').innerHTML = access_token;
-	ping();
-    fs_walk();
-    });
+    fs.walk(renderTree);
+});
 ///
-var _tree = [{text:"Not Loaded"}];
-function getTree() {
-    // Some logic to retrieve, or generate tree structure
-  return _tree;
-}
-function regetTree() {
-    $('#tree').treeview({data: getTree(),levels:0});
-}
-function xregetTree() {
-    $('#tree').treeview({data:getTree(),levels:0,
+var Tree = [{text:"Not Loaded"}];
+function renderTree() {
+    $('#tree').treeview({data:Tree,levels:0,
 		onNodeSelected:function(event,node){
 		LOG("click:"+str(node));
 		if (node.data.cb) node.data.cb(event,node);
@@ -87,22 +67,22 @@ function saveFile(){
     LOG("Save:"+str(fname));
     var text=html2text($('#filebuf').html());
     LOG("Text:"+str(text));
-    fs_save(fname,text,function(){
-	    LOG("Saved.");
-	});
+    fs.save(fname,text,function(){
+	LOG("Saved.");
+    });
 }
 function loadFile(filename){
     LOG("PICKED A FILE "+filename);
-    fs_load(filename,function(data){
-	    LOG("OK GOT IT:1:"+str(data.result));
-	    var newText=text2html(data.result.data);
-	    $( '#dirname'  ).html(data.result.dirname);
-	    $( '#filename' ).html(data.result.filename);
-	    $( '#filebuf'  ).html(newText);
-	});
+    fs.load(filename,function(data){
+	LOG("OK GOT IT:1:"+str(data.result));
+	var newText=text2html(data.result.data);
+	$( '#dirname'  ).html(data.result.dirname);
+	$( '#filename' ).html(data.result.filename);
+	$( '#filebuf'  ).html(newText);
+    });
 }
 function revertFile(){
     var fname=$('#dirname').html()+'/'+$('#filename').html();
     loadFile(fname);
 }
-xregetTree();
+renderTree();
--- static/examples/testamonials/app.js
+++ static/examples/testamonials/app.js
@@ -1,30 +1,34 @@
-function ping(){
-    var access_token = readCookie('access_token');
-    rpc_send("ping",["Hello, world"],function(data){
-	    console.log("PING RESPONSED TO!!!!"+str(data));
-	    document.getElementById('pong').innerHTML = "pong:"+access_token;
+var ps={
+    sub:function(chlist) {
+	rpc_send("sub",[chlist],function(data){
+	    console.log("SUB RESPONSED TO!!!!"+str(data));
 	});
-}
+    },
+    pub:function(ch,msg) {
+	rpc_send("pub",[ch,msg],function(data){
+	    console.log("PUB RESPONSED TO!!!!"+str(data));
+	});
+    }
+};
+///
 function LOG(msg,typ) {
     typ = typ || 'info';
     $('#log').prepend("<p class='alert alert-dismissable small alert-"+typ+"' role='alert'>"+
 		      '<button type="button" class="close" data-dismiss="alert">×</button>'+
-		      msg+
-		      "</p>");
+		      msg+"</p>");
 }
-function QUOTE(msg,who,title,id) {
+
+function add_left_quote(msg,who,title,id) {
     // use this for others' comments
-    LOG("Q1");
     var xstr="<blockquote id='"+id+"'>&ldquo;"+msg+"&rdquo;"+
 	"<footer><cite><abbr title="+title+">"+who+
 	"</abbr></cite></footer></blockquote>";
     $('#add_to').before(xstr);
 }
-function QUOTE2(msg,who,title,id) {
+function add_right_quote(msg,who,title,id) {
     // use this for my comments
     var id2 = id+'__edit';
     var id3 = id+'__who';
-    LOG("id2=["+id2+"]");
     var xstr="<blockquote class='blockquote-reverse' id='"+id+
 	"'>&ldquo;<span id='"+id2+"' contentEditable='true' style='background:cyan'>"+msg+"</span>&rdquo;"+
 	"<footer><cite><abbr contentEditable='true' id='"+id3+"' title="+title+">"+who+
@@ -33,56 +37,29 @@ function QUOTE2(msg,who,title,id) {
     setTimeout(function(){
 	$('#'+id2).focus();
 	$('#'+id2).blur(function(){
-	    LOG("BLUR IT");
 	    var msg = $('#'+id2).html();
 	    var who = $('#'+id3).html();
-	    LOG("BLUR IT1["+id2+"]");
-	    LOG("BLUR IT1["+id3+"]");
-	    rpc_send("pub",["Testamonials",{msg:msg,who:who}],function(data){
-		console.log("PUB RESPONSED TO!!!!"+str(data));
-		///document.getElementById('pong').innerHTML = "pong:"+access_token;
-	    });
-
+	    ps.pub("Testamonials",{msg:msg,who:who});
 	});
     },500);
 }
 ///
-function sub(chlist) {
-    rpc_send("sub",[chlist],function(data){
-	console.log("SUB RESPONSED TO!!!!"+str(data));
-	///document.getElementById('pong').innerHTML = "pong:"+access_token;
-    });
-}
-function pub(ch,msg) {
-    rpc_send("pub",[ch,msg],function(data){
-	console.log("PUB RESPONSED TO!!!!"+str(data));
-	///document.getElementById('pong').innerHTML = "pong:"+access_token;
-    });
-}
-///
-function add_to_convo(x,y,z){
-    QUOTE2("It's great!!!","Random User","Anonymous",nextId());
+function add_to_convo(){
+    add_right_quote("It's great!!!","Random User","Anonymous",nextId());
 }
 
-rpc_add_notify('pub',function(data) {
-    console.log("PUBBBBB"+str(data));
-    var params = data.params;
-    LOG("New Pub: "+str(params));
-    QUOTE(params[0].msg,params[0].who,"Anonymous",nextId());
-});
+function start(){
 
-rpc_add_notify('testimonial',function(data) {
-	console.log("TESTIMONAIL:"+str(data));
-	var result = data.result.replace(/\n/g,'<br>');
-	LOG("New Fortune: "+str(result),'success');
-	document.getElementById('fortune').innerHTML = result;
+    // this pub function will get called for every piece of data coming in
+    rpc_add_notify('pub',function(data) {
+	var params = data.params;
+	add_left_quote(params[0].msg,params[0].who,"Anonymous",nextId());
     });
-rpc_add_open(function(){
-    var access_token = readCookie('access_token');
-    document.getElementById('access_token').innerHTML = access_token;
 
-    rpc_send("ping",["Hello, world"],function(data){
-		console.log("PING RESPONSED TO!!!!"+str(data));
-	    });
-    sub(['Testamonials']);
+    rpc_add_open(function(){
+	ps.sub(['Testamonials']);
     });
+    return this;
+}
+
+start();
