25088bb4665e814f61edcc0c273a8649cd93f6ca
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2015-02-12 00:29:33 -0800

    add rpc client

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ fi
 . .v/bin/activate
 pip install wsaccel ujson
 pip install bottle requests gevent-websocket leveldb markdown2
+pip install websocket-client
 
 alias readme='markdown2 README.md >static/readme.html'
 
